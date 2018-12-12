6e17eb146190e2dcced8ac588dcbc581e0173a78
timkirkicha <tymofiiki@tahometer.com>
2018-12-12 15:29:20 +0200

    Modified: user can calculate values of rectangles (squares, perimeter).

--- rectangles.js
+++ rectangles.js
@@ -5,16 +5,24 @@ function rectangle(color, width, height) {
 }
 
 let rectangles = [
-    new rectangle("black", 1, 2),
+    new rectangle("black", 2, 3),
     new rectangle("red", 3, 3),
     new rectangle("red", 2, 2),
     new rectangle("black", 1, 2),
     new rectangle("black", 2, 1)
 ];
 
-function rectanglesOfColor(color) {
-    return function(item) {
-        return item.filter(isColor(color));
+function calculateValuesOfRectangles(color) {
+    return function (items) {
+        return function (operation) {
+            let result = 0;
+
+            for (let item of items.filter(isColor(color))
+                ) {
+                result += operation(item);
+            }
+            return result;
+        }
     };
 }
 
@@ -24,6 +32,20 @@ function isColor(color) {
     }
 }
 
+//=============================================
+
+let square = function (item) {
+    return item.width * item.height;
+};
+
+let perimeter = function (item) {
+    return 2 * (item.width + item.height)
+};
+
+//=============================================
+
 console.log(
-    rectanglesOfColor("red")(rectangles)
+    // Select rectangles with color
+    calculateValuesOfRectangles("red")(rectangles)(square),
+    calculateValuesOfRectangles("red")(rectangles)(perimeter)
 );
