bcef51d2513d67e88969213f46b7a6ea17331836
timkirkicha <tymofiiki@tahometer.com>
2018-12-12 15:53:39 +0200

    Refactoring code.

--- rectangles.js
+++ rectangles.js
@@ -1,30 +1,26 @@
-function rectangle(color, width, height) {
+function Rectangle(color, width, height) {
     this.color = color;
     this.width = width;
     this.height = height;
 }
 
 let rectangles = [
-    new rectangle("black", 2, 3),
-    new rectangle("black", 3, 3),
-    new rectangle("green", 2, 2),
-    new rectangle("yellow", 1, 2),
-    new rectangle("green", 2, 1)
+    new Rectangle("black", 2, 3),
+    new Rectangle("black", 3, 3),
+    new Rectangle("green", 2, 2),
+    new Rectangle("yellow", 1, 2),
+    new Rectangle("green", 2, 1)
 ];
 
 function calculateValuesOfRectangles(items) {
     return function (color) {
         return function (operation) {
-            let result = 0;
-
-            for (let item of items.filter(isColor(color))
-                ) {
-                result += operation(item);
-            }
-            return result;
+            return calculate(getRectanglesOfColor(items)(color))(operation);
         }
     };
 }
+
+//================================================================
 
 // Predicate
 function isColor(color) {
@@ -33,8 +29,24 @@ function isColor(color) {
     }
 }
 
+function getRectanglesOfColor(items) {
+    return function (color) {
+        return items.filter(isColor(color))
+    }
+}
 
-//=============================================
+function calculate(items) {
+    return function (operation) {
+
+        let result = 0;
+        for (let item of items) {
+            result += operation(item);
+        }
+        return result;
+    }
+}
+
+//================================================================
 // Operations
 
 let square = function (item) {
@@ -45,11 +57,15 @@ let perimeter = function (item) {
     return 2 * (item.width + item.height)
 };
 
-//=============================================
+//================================================================
 
 function findValue(rectangles, color, operation) {
+    console.log(operation.name, 'of', color.toString(), 'rectangles =');
+
     return calculateValuesOfRectangles(rectangles)(color)(operation)
 }
+
+//================================================================
 
 console.log(
     findValue(rectangles, 'black', square)
