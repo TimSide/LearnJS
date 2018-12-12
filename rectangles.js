363bf55ea097689a0bd9b8693899875319a544d6
timkirkicha <tymofiiki@tahometer.com>
2018-12-12 17:14:19 +0200

    Remake project: 1. Select all elements by predicate (color) using method 'filter()'. 2. Get array values of areas of rectangles using method 'map()'. 3. Count sum of values (areas) using method 'reduce()'. 4. Create wrapper for all methods above (flow(filter, map, reduce)).

--- rectangles.js
+++ rectangles.js
@@ -12,65 +12,26 @@ let rectangles = [
     new Rectangle("green", 2, 1)
 ];
 
-function calculateValuesOfRectangles(items) {
-    return function (color) {
-        return function (operation) {
-            return calculate(getRectanglesOfColor(items)(color))(operation);
-        }
-    };
-}
-
 //================================================================
 
-// Predicate
-function isColor(color) {
-    return function (item) {
-        return item.color === color;
-    }
-}
+// Predicate (color)
+let hasColor = predicate => item => item.color === predicate;
 
-function getRectanglesOfColor(items) {
-    return function (color) {
-        return items.filter(isColor(color))
-    }
-}
+let filterByColor = predicate => items => items.filter(hasColor(predicate));
+let calculate = operation => items => items.map(operation);
+let sum = items => items.reduce( (sum, current) => sum + current);
 
-function calculate(items) {
-    return function (operation) {
-
-        let result = 0;
-        for (let item of items) {
-            result += operation(item);
-        }
-        return result;
-    }
-}
+let flow = (color, operation, items) => sum(calculate(operation)(filterByColor(color)(items)));
 
 //================================================================
 // Operations
 
-let square = function (item) {
-    return item.width * item.height;
-};
-
-let perimeter = function (item) {
-    return 2 * (item.width + item.height)
-};
-
-//================================================================
-
-function findValue(rectangles, color, operation) {
-    console.log(operation.name, 'of', color.toString(), 'rectangles =');
-
-    return calculateValuesOfRectangles(rectangles)(color)(operation)
-}
+let area = item => item.width * item.height;
+let perimeter = item => 2 * (item.width + item.height);
 
 //================================================================
 
 console.log(
-    findValue(rectangles, 'black', square)
-);
-
-console.log(
-    findValue(rectangles, 'black', perimeter)
+    flow("black", area, rectangles),
+    flow("black", perimeter, rectangles)
 );
